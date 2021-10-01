d2913644058af8e94f7a091e688bef9f646a514a
nguyenminhquy1624 <minhquy1624@gmail.com>
2021-10-01 15:50:13 +0700

    update w1 to week3

---
+++ week1/HelloWorld.java
@@ -0,0 +1,12 @@
+import java.util.Iterator;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		for(int i = 0 ; i < 10 ; i ++)
+		{
+			System.out.println("Hello World");
+		}
+	}
+
+}
